12319fd91658923bbc30973377c8947f257a5668
nyarismati <morismorrison22@gmail.com>
2021-09-15 07:07:38 +0700

    first commit

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+
+    private AudioSource audioPlayer;
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    void Update()
+    {
+        
+    }
+}
